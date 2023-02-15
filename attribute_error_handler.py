59e875d7e032baceca1d256095b10541b6c3d2e8
乡ＴＯＢＹ乡 <80438941+umrbek1032@users.noreply.github.com>
2023-02-16 02:54:47 +0500

    removed unused files

--- attribute_error_handler.py
+++
@@ -1,7 +0,0 @@
-try:
-    badContent = bsObj.nonExistingTag.anotherTag
-except AttributeError as e:
-    if badContent == None:
-        print("Tag could not be found")
-    else:
-        print(badContent)
